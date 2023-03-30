d14abc6658cf0e583a9327398e3777e28aeac742
Althail <72927787+Althail@users.noreply.github.com>
2023-03-30 23:31:15 +0200

    Test

---
+++ src/test/java/fr/epsi/entity/AuctionDAOTest.java
@@ -0,0 +1,12 @@
+package fr.epsi.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuctionDAOTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
